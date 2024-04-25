830951ed49524922e5fd94c7112c93270f6a114d
TaitKaito <clayton.h.c123@gmail.com>
2024-04-26 00:49:44 +0100

    Fix 105Sap shrap calling

--- Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/105mm SAP.cs
+++ Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/105mm SAP.cs
@@ -390,7 +390,7 @@ namespace Scripts
         private AmmoDef SapShell105Srap => new AmmoDef // Your ID, for slotting into the Weapon CS
         {
             AmmoMagazine = "Energy", // SubtypeId of physical ammo magazine. Use "Energy" for weapons without physical ammo.
-            AmmoRound = "5InchSapShrap", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
+            AmmoRound = "105InchSapShrap", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
             HybridRound = false, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 0.1f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
             BaseDamage = 450f, // Direct damage; one steel plate is worth 100.
